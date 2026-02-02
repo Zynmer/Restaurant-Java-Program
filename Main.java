58cdf1329858b7686ddb4b1e2efd28e7ba47fecd
Zynmer <pryderek@gmail.com>
2026-02-02 11:22:21 -0700

    Add files via upload
    Updated Main.

--- Main.java
+++ Main.java
@@ -6,7 +6,10 @@ public class Main {
         Restaurant chipotle = new Restaurant(30, 4.5, 14, 5, "6:00am - 9:00pm");
 
         System.out.println(chipotle.avgPrice);
-
+        System.out.println(chipotle.rating);
+        System.out.println(chipotle.waitTime);
+        System.out.println(chipotle.distance);
+        System.out.println(chipotle.hoursOpen);
     
     }
 }
