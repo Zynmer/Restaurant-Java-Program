853412d4959ee8595d2b4531a6bfbbde0be4308c
Zynmer <pryderek@gmail.com>
2026-02-25 10:59:32 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,30 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        
-        Restaurant mcDonalds = new Restaurant();
-        Restaurant chipotle = new Restaurant(30, 4.5, 14, 5, "6:00am - 9:00pm");
 
-        System.out.println(chipotle.avgPrice);
-        System.out.println(chipotle.rating);
-        System.out.println(chipotle.waitTime);
-        System.out.println(chipotle.distance);
-        System.out.println(chipotle.hoursOpen);
+        //Instantiate ArrayList to hold Restaurant Objects
+        ArrayList<Restaurant> restaurantList = new ArrayList<>();
+
+        //Take user input using JOptionPane and store in a variable
+        String name = JOptionPane.showInputDialog("Enter restaurant name: ");
+        int waitTime = Integer.parseInt(JOptionPane.showInputDialog("Enter wait time in number of minutes (ex. 30): "));
+        double rating = Double.parseDouble(JOptionPane.showInputDialog("Enter rating (ex. 5.0): "));
+        double avgPrice = Double.parseDouble(JOptionPane.showInputDialog("Enter average price to eat (ex. 12.50): "));
+        double distance = Double.parseDouble(JOptionPane.showInputDialog("Enter distance to restaurant in miles (ex. 5.7): "));
+        String hoursOpen = JOptionPane.showInputDialog("Enter hours open (ex. 0900 - 1700): ");
+
+        //Instantiate Restaurant Object with variables
+        Restaurant userObject = new Restaurant(name, waitTime, rating, avgPrice, distance, hoursOpen);
+        System.out.println(userObject);
+
+        //Add user's Object to array list
+        restaurantList.add(userObject);
+        
     
     }
 }
+
+
--- Restaurant.java
+++ Restaurant.java
@@ -1,21 +1,112 @@
 public class Restaurant {
     
     // Properties
-    int waitTime;
-    double rating;
-    double avgPrice;
-    double distance;
-    String hoursOpen;
+    private String Name;
+    private int WaitTime;
+    private double Rating;
+    private double AvgPrice;
+    private double Distance;
+    private String HoursOpen;
  
     //Constructer
     public Restaurant() {}
 
-    public Restaurant(int waitTime, double rating, double avgPrice, double distance, String hoursOpen) {
-        this.waitTime = waitTime;
-        this.rating = rating;
-        this.avgPrice = avgPrice;
-        this.distance = distance;
-        this.hoursOpen = hoursOpen;
+    public Restaurant(String Name, int WaitTime, double Rating, double AvgPrice, double Distance, String HoursOpen) {
+        
+        this.WaitTime = WaitTime;
+        this.Rating = Rating;
+        this.AvgPrice = AvgPrice;
+        this.Distance = Distance;
+        this.HoursOpen = HoursOpen;
+        this.Name = Name;
 
     }
+    
+    // Getters and Setters
+    public int getWaitTime() {
+    
+        return WaitTime;
+
+    }
+   
+    public void setWaitTime(int WaitTime) {
+    
+        this.WaitTime = WaitTime;
+
+    }
+
+    public double getRating() {
+
+        return Rating;
+
+    }
+
+    public void setRating(double Rating) {
+
+        this.Rating = Rating;
+
+    }
+
+    public double getAvgPrice() {
+
+        return AvgPrice;
+
+    }
+
+    public void setAvgPrice(double AvgPrice) {
+
+        this.AvgPrice = AvgPrice;
+
+    }
+
+    public double getDistance() {
+
+        return Distance;
+
+    }
+
+    public void setDistance(double Distance) {
+
+        this.Distance = Distance;
+
+    }
+
+    public String getHoursOpen() {
+
+        return HoursOpen;
+
+    }
+
+    public void setHoursOpen(String HoursOpen) {
+
+        this.HoursOpen = HoursOpen;
+
+    }
+
+    public String getName() {
+
+        return Name;
+
+    }
+
+    public void setName(String Name) {
+
+        this.Name = Name;
+
+    }
+
+    public String toString() {
+
+        String output = String.format("Restaurant Name: %s%n"
+                                    + "Wait Time:       %d%n"
+                                    + "Rating:          %.1f%n"
+                                    + "Average Price    %.1f%n"
+                                    + "Distance:        %.1f%n"
+                                    + "Hours Open:      %s%n", 
+                                    Name, WaitTime, Rating, AvgPrice, Distance, HoursOpen);
+
+        return output;
+
+    }
+
 }
